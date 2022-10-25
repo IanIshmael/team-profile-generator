124bcaabcb974aab2cf9c4d89f5b651bcd748af7
IanIshmael <107890250+IanIshmael@users.noreply.github.com>
2022-10-25 15:39:19 -0600

    Wrote classes for all file in lib folder. Connected template literals to htmltemplate.js. Created template.html to put htmltemplate.js on a html file. Wrote the majority of index.js but still need to generate html. I also need to write all the tests.

---
+++ index.js
@@ -0,0 +1,138 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+const Manager = require("./lib/manager");
+const Engineer = require("./lib/engineer");
+const Intern = require("./lib/intern");
+const generateHtml = require('./src/htmltemplate')
+const createdEmployees = [];
+
+const start = function () {
+  inquirer
+    .prompt([
+      {
+        name: "name",
+        type: "input",
+        message: "What is your team managers name?",
+      },
+      {
+        name: "id",
+        type: "number",
+        message: "What is your team managers id?",
+      },
+      {
+        name: "email",
+        type: "input",
+        message: "What is your team managers email address?",
+      },
+      {
+        name: "number",
+        type: "number",
+        message: "What is your team managers office number?",
+      },
+    ])
+    .then((response) => {
+      console.log(response);
+      const newManager = new Manager(
+        response.name,
+        response.id,
+        response.email,
+        response.number
+      );
+      createdEmployees.push(newManager);
+      askedNext();
+    });
+};
+const askedNext = function () {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "choice",
+        message: "Select an engineer, intern, or generate team.",
+        choices: ["engineer", "intern", "generate team"],
+      },
+    ])
+    .then((answers) => {
+      if (answers.choice === "engineer") {
+        createEngineer();
+      } else if (answers.choice === "intern") {
+        createIntern();
+      } else {
+        createHtml();
+      }
+    });
+};
+const createEngineer = function () {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "What is your team engineers name?",
+      },
+      {
+        type: "input",
+        name: "id",
+        message: "What is your team engineers id?",
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "Enter your team engineers email address.",
+      },
+      {
+        type: "input",
+        name: "github",
+        message: "What is your team engineers github name?",
+      },
+    ])
+    .then((answers) => {
+        const engineer = new Engineer(
+          answers.name,
+          answers.id,
+          answers.email,
+          answers.github
+        );
+        createdEmployees.push(engineer);
+        askedNext();
+    });
+};
+
+const createIntern = function () {
+    inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "What is your interns name?",
+      },
+      {
+        type: "input",
+        name: "id",
+        message: "What is your interns id?",
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "Enter your interns email address.",
+      },
+      {
+        type: "input",
+        name: "school",
+        message: "What is your interns school?",
+      },
+    ])
+    .then((answers) => {
+      const intern = new Intern(
+        answers.name,
+        answers.id,
+        answers.email,
+        answers.school
+      );
+      createdEmployees.push(intern);
+      askedNext();
+    });
+};
+
+
+start();
--- src/htmltemplate.js
+++ src/htmltemplate.js
@@ -1,1 +1,85 @@
 //!! function to generate each team menbers html
+const generateManager = function (manager) {
+  return `
+        <div class="col">
+          <div class="card h-100 ">
+            <div class="card-body text-bg-primary">
+              <h5 class="card-title text-center">${manager.getName}</h5>
+              <p class="card-text">
+                ${manager.getRole}
+              </p>
+            </div>
+            <div class="card-footer">
+              <div class="card" style="width: 18rem">
+                <ul class="list-group list-group-flush">
+                  <li class="list-group-item">ID:${manager.getId}</li>
+                  <li class="list-group-item">Email:<a href="#">${manager.getEmail}</a></li>
+                  <li class="list-group-item">
+                    ${manager.getofficeNumber}
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+        `;
+};
+const generateEnginer = function (engineer) {
+  return `
+
+            <div class="col">
+              <div class="card h-100 ">
+                <div class="card-body text-bg-primary">
+                  <h5 class="card-title text-center">${engineer.getName}</h5>
+                  <p class="card-text">
+                    ${engineer.getRole}
+                  </p>
+                </div>
+                <div class="card-footer">
+                  <div class="card" style="width: 18rem">
+                    <ul class="list-group list-group-flush">
+                      <li class="list-group-item">ID:${engineer.getId}</li>
+                      <li class="list-group-item">Email:<a href="#">${engineer.getEmail}</a></li>
+                      <li class="list-group-item">
+                        GitHub:<a href="#">${engineer.getGithub}</a>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+            `;
+};
+
+const generateIntern = function (intern) {
+  return `
+   
+            <div class="col">
+              <div class="card h-100 ">
+                <div class="card-body text-bg-primary">
+                  <h5 class="card-title text-center">${intern.getName}</h5>
+                  <p class="card-text">
+                    ${intern.getRole}
+                  </p>
+                </div>
+                <div class="card-footer">
+                  <div class="card" style="width: 18rem">
+                    <ul class="list-group list-group-flush">
+                      <li class="list-group-item">ID:${intern.getId}</li>
+                      <li class="list-group-item">Email:<a href="#">${intern.getEmail}</a></li>
+                      <li class="list-group-item">
+                        School:<a href="#">${intern.getSchool}</a>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+            `;
+};
+
+module.exports = {
+  generateManager,
+  generateEnginer,
+  generateIntern,
+};
